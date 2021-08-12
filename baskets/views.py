a500853c3d3a808c1d19a3eb70c3aa650ae4adba
Kulkov Nikita_REG <kulkovnik93@gmail.com>
2021-08-12 14:07:55 +0300

    added recover version

--- baskets/views.py
+++ baskets/views.py
@@ -14,6 +14,7 @@ def basket_add(request, product_id):
     product = Product.objects.get(id=product_id)
     baskets = Basket.objects.filter(user=request.user, product=product)
 
+
     if not baskets.exists():
         Basket.objects.create(user=request.user, product=product, quantity=1)
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
--- orders/forms.py
+++ orders/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from orders.models import Order, OrderItem
-
+from products.models import Product
 
 class OrderForm(forms.ModelForm):
     class Meta:
@@ -26,3 +26,5 @@ class OrderItemForm(forms.ModelForm):
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
+
+        self.fields['product'].queryset = Product.objects.all().select_related()
